5a4d0577274369ae191bf5db2bb738961892b6d8
선명준 <sun950903@gmail.com>
2022-01-18 23:39:04 +0900

    2022.01.18 | 15654 | N과 M(5)

---
+++ algorithm_study/15654_N과 M(5)/BOJ_15654_N과 M(5).java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,M;
+	static int[] result,num;
+	static boolean[] check;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		result = new int[N];
+		num = new int[N];
+		check = new boolean[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
+			num[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(num);
+		dfs(0);
+		System.out.println(sb.toString());
+	}
+    
+	static void dfs(int cnt) {
+		if(cnt == M) {
+			for(int i=0; i<M; i++)
+				sb.append(result[i]).append(" ");
+			sb.append("\n");
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			if(check[i] == false) {
+				check[i] = true;
+				result[cnt] = num[i];
+				dfs(cnt+1);
+				check[i] = false;
+			}
+		}
+	}
+}
