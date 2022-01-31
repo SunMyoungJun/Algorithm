b9722ae3e1d228f5703c07ad475faeb6e1e0a6a6
선명준 <sun950903@gmail.com>
2022-01-31 22:22:03 +0900

    2022.01.31 | 15665 | N과 M(11)

---
+++ algorithm_study/15665_N과 M(11)/BOJ_15665_N과 M(11).java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+	static int N,M;
+	static int[] result,num;
+	static boolean[] check;
+	static TreeSet<String> treeSet = new TreeSet<>();
+	static StringBuilder sb = new StringBuilder();
+	static StringBuilder sb2 = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		result = new int[N];
+		num = new int[N];
+		check = new boolean[N];
+		st = new StringTokenizer(br.readLine());
+
+		for(int i=0; i<N; i++) 
+			num[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(num);
+		dfs(0);
+		System.out.println(sb2.toString());
+	}
+
+	static void dfs(int cnt) {
+		if(cnt == M) {
+			sb.setLength(0);
+			for(int i=0; i<M; i++) {
+				sb.append(result[i]).append(" ");
+			}
+			if(!treeSet.contains(sb.toString())) {
+				sb2.append(sb.toString()).append("\n");
+			}
+			treeSet.add(sb.toString());
+			return;
+		}
+
+		for(int i=0; i<N; i++) {
+			result[cnt] = num[i];
+			dfs(cnt+1);
+		}
+	}
+}
