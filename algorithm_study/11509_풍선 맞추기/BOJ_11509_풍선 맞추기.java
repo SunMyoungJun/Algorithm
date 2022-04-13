3ea97c3aa04581c23a94c4c24e330fba1ab2bbfc
선명준 <sun950903@gmail.com>
2022-04-13 16:35:47 +0900

    2022.04.13 | 11509 |  풍선 맞추기

---
+++ algorithm_study/11509_풍선 맞추기/BOJ_11509_풍선 맞추기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N= Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int cnt=0;
+		int[] map = new int[N];
+		int[] num = new int[1000002];
+		for(int i=0; i<N; i++) {
+			map[i] = Integer.parseInt(st.nextToken());
+			if(num[map[i]+1] == 0) {
+				cnt++;
+			}
+			else {
+				num[map[i]+1]--;
+			}
+			num[map[i]]++;
+		}
+		System.out.println(cnt);
+	}
+}
